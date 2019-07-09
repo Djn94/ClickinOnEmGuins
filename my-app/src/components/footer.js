b4dba86bde333c15b16d0eab9d2be77dbf7541db
Non Dewman <Dnewman1934@yahoo.com>
2019-07-09 16:49:57 -0500

    working sticky footer

--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -2,14 +2,32 @@ import React from 'react';
 const style = {
     footerStyle: {
         background: 'blue',
-        height: 75,
-        textAlign: "center",
+        fontSize: 20,
+        color: "black",
+        borderTop: "3px solid black",
+        textAlign: 'center',
+        padding: 20,
+        position: "fixed",
+        left: "0",
+        bottom: "0",
+        height: 60,
+        width: "100%",
+    },
+    ghostdiv: {
+        display: "block",
+        padding: "20px",
+        height: 60,
+        width: "100%",
     }
 }
 function Footer() {
-    return (<div style={style.footerStyle} className='footerDiv'>
-        <h4>Copyright 2019</h4>
-    </div >
+    return (
+        <div>
+            <div style={style.ghostdiv} />
+            <div style={style.footerStyle}>
+                <span id="copyrightText">Copyright 2019</span>
+            </div>
+        </div>
     );
 }
 export default Footer;
